33c54db8178e0a747961cade6eaedbfb3119d116
Alexander Güntert <alexander.guentert@gmail.com>
2024-11-22 08:39:40 +0100

    add sources personenwagen

---
+++ sources/needful_things/pw_bestand.sql
@@ -0,0 +1,2 @@
+SELECT * FROM read_csv("https://data.stadt-zuerich.ch/dataset/prd_ssz_fz_pw_bestand_jahr_quartier_antriebsart_rechtsform_od2001/download/VER200OD2001.csv");
+
---
+++ sources/needful_things/pw_neu.sql
@@ -0,0 +1,2 @@
+SELECT * FROM read_csv("https://data.stadt-zuerich.ch/dataset/prd_ssz_fz_pw_neuzulassungen_jahr_quartier_antriebsart_rechtsform_od2004/download/VER200OD2004.csv");
+
